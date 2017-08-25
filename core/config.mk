3a35562c98dd4117690499712caf64363bc18fbd
ezio84 <brabus84@gmail.com>
2017-08-25 10:26:20 +0200

    brunch/breakfast: fix path and set userdebug
    Change-Id: Icba3c57f1d47a6b92b953d9b4db2897bc4928d7d

--- core/config.mk
+++ core/config.mk
@@ -888,7 +888,7 @@ endif
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/nexus/sepolicy/sepolicy.mk)
+$(eval include vendor/six/sepolicy/sepolicy.mk)
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
